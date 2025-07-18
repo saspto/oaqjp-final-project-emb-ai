daa744075a5c24aea09b9a0ab5e6ed0403911918
sas <sas@example.com>
2025-07-18 14:38:30 -0400

    task6

---
+++ server.py
@@ -0,0 +1,38 @@
+''' Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+# Import Flask, render_template, request from the flask pramework package : 
+from flask import Flask, render_template, request
+# Import the emotion_detector function from the package created:
+from EmotionDetection.emotion_detection import emotion_detector
+
+
+#Initiate the flask app : TODO
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def emotion_detector():
+    '''Emotion Detector
+    '''
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the emotion_detector function and store the response
+    response = emotion_detector(text_to_analyze)
+
+    # Extract from the response
+    if response.status_code == 200:
+        dominant_emotion = response['dominant_emotion']
+        return "For the given statement, the system response is {}. The dominant emotion is {}.", response, dominant_emotion
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    # This functions executes the flask app and deploys it on localhost:5000
+    app.run(host="0.0.0.0", port=5000)
